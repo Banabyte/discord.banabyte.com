64d7047bf02f3d0ce60a139e30bbbd5fe82a69bd
ZEROPOINT <wegj1@hotmail.com>
2024-08-26 11:35:59 -0600

    Second update

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
             const serverInfo = document.getElementById('server-info');
             // Fetch the total number of members from the widget data
             serverInfo.innerHTML = `
-                <p><i class="fas fa-users"></i> Total Members: ${data.presence_count}</p>
+                <p><i class="fas fa-users"></i> Members Active: ${data.presence_count}</p>
                 <p><i class="fas fa-microphone"></i> ${data.channels.length} Channels</p>
             `;
         })
